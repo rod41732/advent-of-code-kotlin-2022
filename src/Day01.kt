c6875b12807defdd220c67a15ccf86a73a51ff2d
road <rod8711@gmail.com>
2022-12-02 11:09:45 +0700

    optmized solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,28 @@
+import java.util.*
+
 fun main() {
     val elfCalories = readInputRaw("Day01").trim() // prevent trailing endline from causing problem
         .split("\n\n")
         .map { it.split("\n").sumOf { it.toInt() } }
 
+    fun part1(eachElfCalories: List<Int>) = eachElfCalories.max()
+    fun part2(eachElfCalories: List<Int>) = eachElfCalories.sortedDescending().take(3).sum()
+    fun part2Faster(eachElfCalories: List<Int>): Int {
+        val heap = PriorityQueue(Int::compareTo)
+        eachElfCalories.forEach {
+            if (heap.size != 0 && it < heap.peek()) return@forEach
+            heap.add(it)
+            if (heap.size > 3) heap.remove()
+        }
+        return heap.sum()
+    }
+
     println("Part 1")
-    println(elfCalories.max())
+    println(part1(elfCalories))
 
     println("Part 2")
-    println(elfCalories.sortedDescending().take(3).sum())
+    println(part2(elfCalories))
+
+    println("Part 2 - O(N) solution")
+    println(part2Faster(elfCalories))
 }
