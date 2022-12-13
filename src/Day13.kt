988ad2326f441a273fd1dc7af5015557075accfd
road <rod8711@gmail.com>
2022-12-13 22:44:44 +0700

    clean up 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,73 +1,18 @@
-import javax.swing.plaf.ColorUIResource
-
-private sealed class Packet {
-    data class PacketList(
-        val parent: PacketList?,
-        val children: MutableList<Packet> = mutableListOf()
-    ) :
-        Packet(), Comparable<PacketList> {
-        override fun compareTo(other: PacketList): Int {
-            val elementCmp = children.asSequence()
-                .zip(other.children.asSequence()) { left, right -> left.compareTo(right) }
-                .firstOrNull { it != 0 }
-            return elementCmp ?: children.size.compareTo(other.children.size)
-        }
-
-        fun addElement(value: Int) { children.add(PacketNumber(value)) }
-        fun addList(): PacketList = PacketList(this).also { children.add(it) }
-        override fun print(indent: Int) {
-            println("  ".repeat(indent)  + "List (${children.size} elements)")
-        }
-    }
-
-    data class PacketNumber(val value: Int) : Packet(), Comparable<PacketNumber> {
-        override fun compareTo(other: PacketNumber): Int = value.compareTo(other.value)
-        override fun print(indent: Int) {
-            println("  ".repeat(indent)  + "Item: $value")
-        }
-    }
-
-    fun compareTo(other: Packet): Int {
-        if (this is PacketList || other is PacketList) return this.asPacketList().compareTo(other.asPacketList())
-        return (this as PacketNumber).compareTo(other as PacketNumber)
-    }
-
-    fun asPacketList(): PacketList = if (this is PacketList) this else PacketList(null, mutableListOf(this))
-
-    abstract fun print(indent: Int = 0)
-}
-
-
-
 fun main() {
     val input = readInput("Day13")
     val testInput = readInput("Day13_test")
 
     fun part1(input: List<String>): Int {
-        return input.chunked(3)
-            .map { it.take(2).map(::parseList) }
-            .map { (left, right) -> left.compareTo(right) }
-            .withIndex()
-            .filter { (_, cmp) -> cmp == -1 }
-            .map { (idx) -> idx + 1 }
-//            .also { println("correct order: $it ")}
-            .sum()
+        return input.filter { it.isNotBlank() }.map(::parseList).chunked(2)
+            .sumOfIndexed { idx, (x, y) -> if (x.compareTo(y) == -1) idx + 1 else 0 }
     }
 
     fun part2(input: List<String>): Int {
-        val dividers = listOf(
-            parseList("[[2]]"),
-            parseList("[[6]]")
-        )
-        val all = input
-            .filter { it.isNotBlank() }
-            .map(::parseList)
-            .plus(dividers)
-            .sorted()
-        return dividers.map { all.binarySearch(it) + 1} .reduce { acc, i -> acc * i  }
-
-
+        val dividers = listOf(parseList("[[2]]"), parseList("[[6]]"))
+        val all = input.filter { it.isNotBlank() }.map(::parseList).plus(dividers).sorted()
+        return dividers.map { all.binarySearch(it) + 1 }.reduce { acc, i -> acc * i }
     }
+
     println(part1(testInput))
     check(part1(testInput) == 13)
 
@@ -84,13 +29,15 @@ fun main() {
 
 private data class NumberBuilder(var currentNum: Int = 0, var hasNum: Boolean = false) {
     fun addDigit(digit: Int) {
-        currentNum =  currentNum * 10 + digit
+        currentNum = currentNum * 10 + digit
         hasNum = true
     }
-    fun collectNumber() = if (hasNum) currentNum.also { hasNum = false } else null
+
+    fun collectNumber() = if (hasNum) currentNum.also { hasNum = false; currentNum = 0 } else null
 }
-private fun parseList(inp: String): Packet.PacketList {
-    val root = Packet.PacketList(null)
+
+private fun parseList(inp: String): Packet {
+    val root = PacketList(null)
     var currentList = root
     val numberBuilder = NumberBuilder()
 
@@ -101,13 +48,41 @@ private fun parseList(inp: String): Packet.PacketList {
                 numberBuilder.collectNumber()?.also { currentList.addElement(it) }
                 currentList = currentList.parent!!
             }
-            ',' -> numberBuilder.collectNumber()?.also { currentList.addElement(it) }
 
+            ' ' -> {}
+            ',' -> numberBuilder.collectNumber()?.also { currentList.addElement(it) }
             else -> numberBuilder.addDigit(it - '0')
         }
     }
-    return root.children[0] as Packet.PacketList
+    return root.children[0]
+}
+
+private sealed class Packet : Comparable<Packet> {
+    override fun compareTo(other: Packet): Int {
+        if (this is PacketNumber && other is PacketNumber) return value.compareTo(other.value)
+        return compareList(asPacketList(), other.asPacketList())
+    }
+
+    fun compareList(left: PacketList, right: PacketList): Int {
+        left.children.zip(right.children)
+            .forEach { (left, right) -> left.compareTo(right).let { if (it != 0) return it } }
+        return left.children.size.compareTo(right.children.size)
+    }
+
+    fun asPacketList(): PacketList = if (this is PacketList) this else PacketList(null, mutableListOf(this))
+}
+
+private data class PacketList(
+    val parent: PacketList?, val children: MutableList<Packet> = mutableListOf()
+) : Packet() {
+
+    fun addElement(value: Int) {
+        children.add(PacketNumber(value))
+    }
+
+    fun addList(): PacketList = PacketList(this).also { children.add(it) }
 }
 
 
+private data class PacketNumber(val value: Int) : Packet()
 
